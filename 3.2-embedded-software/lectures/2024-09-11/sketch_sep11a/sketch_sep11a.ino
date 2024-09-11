510587d5d90fd45c69dae6db06b8b36c8fc6215d
Jonghyeon Park <belline0124@gmail.com>
2024-09-11 12:03:44 +0900

    embedded 2024-09-11

---
+++ 3.2-embedded-software/lectures/2024-09-11/sketch_sep11a/sketch_sep11a.ino
@@ -0,0 +1,33 @@
+#define PIN_LED 9
+#define PIN_SW_UP 13
+#define PIN_SW_DN 12
+#define LIGHT_DEFAULT 128
+#define LIGHT_DIFF_UP 1
+#define LIGHT_DIFF_DN 1
+#define LIGHT_MIN 1
+#define LIGHT_MAX 255
+#define SERIAL_SPEED 9600
+
+int ledLight = LIGHT_DEFAULT;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_SW_UP, INPUT);
+  pinMode(PIN_SW_DN, INPUT_PULLUP);
+  Serial.begin(SERIAL_SPEED);
+  Serial.println("Init done.");
+}
+
+void loop() {
+  int isUpSwPressed = digitalRead(PIN_SW_UP);
+  if (isUpSwPressed) ledLight++;
+  int isDnSwPressed = !digitalRead(PIN_SW_DN);
+  if (isDnSwPressed) ledLight--;
+
+  Serial.println("isUpSwPressed: " + String(isUpSwPressed) + ", isDnSwPressed: " + String(isDnSwPressed) + ", ledLight: " + String(ledLight));
+
+  if (ledLight < LIGHT_MIN) ledLight = LIGHT_MIN;
+  if (ledLight > LIGHT_MAX) ledLight = LIGHT_MAX;
+  
+  analogWrite(PIN_LED, ledLight);
+}
