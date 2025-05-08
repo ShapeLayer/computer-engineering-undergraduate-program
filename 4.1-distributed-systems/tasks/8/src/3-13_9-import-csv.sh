82ba4faa34edf27b6a5c3505bcecaf549c2a1cd7
Jonghyeon Park <belline0124@gmail.com>
2025-05-08 20:27:56 +0900

    dist: task 08

--- 4.1-distributed-systems/tasks/8/src/3-13_9-import-csv.sh
+++ 4.1-distributed-systems/tasks/8/src/3-13_9-import-csv.sh
@@ -1,15 +1,15 @@
 hdfs dfs -put ./emp.csv /output
-echo 'create 'employees', 'personal', 'department'' | hbase shell
+echo 'create `employees`, `personal`, `department`' | hbase shell
 
 hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.separator="," -Dimporttsv.columns=HBASE_ROW_KEY,personal:name,department:role employees /output/emp.csv
 
 
 # Page 10
-echo 'create 'employees_copy', 'personal', 'department'' | hbase shell
+echo 'create `employees_copy`, `personal`, `department`' | hbase shell
 hbase org.apache.hadoop.hbase.mapreduce.CopyTable -new.name=employees_copy employees
 
 
 # Page 11
 hbase org.apache.hadoop.hbase.mapreduce.Export employees /output/employees
-echo 'create 'employees_import', 'personal', 'department'' | hbase shell
+echo 'create `employees_import`, `personal`, `department`' | hbase shell
 hbase org.apache.hadoop.hbase.mapreduce.Import employees_import /output/employees
