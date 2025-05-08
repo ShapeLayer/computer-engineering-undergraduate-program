82ceac8389c2ec172ac1cc5a6e57c651212830eb
Jonghyeon Park <belline0124@gmail.com>
2025-05-08 14:35:08 +0900

    web: 2025-05-08

---
+++ 3.1-web-programming/lectures/2025-05-08/array.js
@@ -0,0 +1,10 @@
+// 자바스크립트에서는 배열도 객체임
+
+let literal = [1, 2, 3, 4, 5]
+let arrObj = new Array(1, 2, 3, 4, 5)
+let arrObj2 = new Array(5)
+arrObj2[0] = 1
+arrObj2[1] = 2
+arrObj2[2] = 3
+arrObj2[3] = 4
+arrObj2[4] = 5
